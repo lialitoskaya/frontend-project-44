588d60d3b2e24082f69c092aade87781f13d001d
liya <liyarheon@gmail.com>
2023-02-08 16:12:21 +0300

    add dir games, new correct brain-games layout

---
+++ bin/games/brain-calc.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+import { brain_calc, hiUser, welcome } from '../../src/index.js';
+
+welcome();
+hiUser();
+brain_calc();
---
+++ bin/games/brain-games.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+import { welcome, hiUser, userName } from '../../src/index.js';
+
+welcome();
+hiUser();
