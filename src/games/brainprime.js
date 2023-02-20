599257bb1f1524486035a04b633477b5b20319dd
liya <liyarheon@gmail.com>
2023-02-20 20:31:29 +0300

    update brainprime.js

--- src/games/brainprime.js
+++ src/games/brainprime.js
@@ -12,17 +12,14 @@ const brainPrime = () => {
   while (i < 3) {
     const questionNum = randomNum(1, 10);
     console.log(`${question}${questionNum}`);
-
     let count = 0;
     let correctAnsw;
-
     for (let i1 = 2; i1 <= questionNum; i1 += 1) {
       if (questionNum % i1 === 0) {
         count += 1;
       }
       correctAnsw = count > 1 || questionNum === 1 ? 'no' : 'yes';
     }
-
     const userAnswer = readlineSync.question(answer);
     trueAnswer(correctAnsw, userAnswer);
     if (correctAnsw !== userAnswer) {
