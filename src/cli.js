f4df24d02985a6443393d04be82ede162a0c5adc
liya <liyarheon@gmail.com>
2023-03-06 14:50:26 +0300

    update welcome function

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,11 @@
 import readlineSync from 'readline-sync';
 // eslint-disable-next-line import/no-mutable-exports
-let userName;
+
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
-  userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export default welcome;
-export { userName };
